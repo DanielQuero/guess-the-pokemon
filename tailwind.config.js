c46885ab39c1e3e193781f0516006637f45406e0
Daniel Quero <danielquero95@gmail.com>
2023-12-20 17:36:35 +0100

    Clean styles, added new font and added new styles for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'not-hover-on-phones': {'raw': '(hover: hover)'},
+      },
       transitionDuration: {
         '2000': '2000ms',
         '3000': '3000ms',
